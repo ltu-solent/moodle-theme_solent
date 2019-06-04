6260d9e17253f889614b4bfbd2735869398e93db
Oliver Kent <okent@spartaglobal.com>
2019-06-04 16:24:49 +0100

    Fixed settings menu

--- settings.php
+++ settings.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package   theme_solent
+ * @package   theme_boost
  * @copyright 2016 Ryan Wyllie
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -23,13 +23,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-    $settings = new theme_solent_admin_settingspage_tabs('themesettingsolent', get_string('configtitle', 'theme_solent'));
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingsolent', get_string('configtitle', 'theme_solent'));
     $page = new admin_settingpage('theme_solent_general', get_string('generalsettings', 'theme_solent'));
 
     // Preset.
     $name = 'theme_solent/preset';
     $title = get_string('preset', 'theme_solent');
-    $description = get_string('preset_desc', 'theme_solent');
+    $description = get_string('preset_desc', 'theme_boost');
     $default = 'default.scss';
 
     $context = context_system::instance();
@@ -50,8 +50,8 @@ if ($ADMIN->fulltree) {
 
     // Preset files setting.
     $name = 'theme_solent/presetfiles';
-    $title = get_string('presetfiles','theme_solent');
-    $description = get_string('presetfiles_desc', 'theme_solent');
+    $title = get_string('presetfiles','theme_boost');
+    $description = get_string('presetfiles_desc', 'theme_boost');
 
     $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
         array('maxfiles' => 20, 'accepted_types' => array('.scss')));
@@ -59,8 +59,8 @@ if ($ADMIN->fulltree) {
 
     // Background image setting.
     $name = 'theme_solent/backgroundimage';
-    $title = get_string('backgroundimage', 'theme_solent');
-    $description = get_string('backgroundimage_desc', 'theme_solent');
+    $title = get_string('backgroundimage', 'theme_boost');
+    $description = get_string('backgroundimage_desc', 'theme_boost');
     $setting = new admin_setting_configstoredfile($name, $title, $description, 'backgroundimage');
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
@@ -68,8 +68,8 @@ if ($ADMIN->fulltree) {
     // Variable $body-color.
     // We use an empty default value because the default colour should come from the preset.
     $name = 'theme_solent/brandcolor';
-    $title = get_string('brandcolor', 'theme_solent');
-    $description = get_string('brandcolor_desc', 'theme_solent');
+    $title = get_string('brandcolor', 'theme_boost');
+    $description = get_string('brandcolor_desc', 'theme_boost');
     $setting = new admin_setting_configcolourpicker($name, $title, $description, '');
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
@@ -82,13 +82,13 @@ if ($ADMIN->fulltree) {
 
     // Raw SCSS to include before the content.
     $setting = new admin_setting_scsscode('theme_solent/scsspre',
-        get_string('rawscsspre', 'theme_solent'), get_string('rawscsspre_desc', 'theme_solent'), '', PARAM_RAW);
+        get_string('rawscsspre', 'theme_boost'), get_string('rawscsspre_desc', 'theme_boost'), '', PARAM_RAW);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
 
     // Raw SCSS to include after the content.
-    $setting = new admin_setting_scsscode('theme_solent/scss', get_string('rawscss', 'theme_solent'),
-        get_string('rawscss_desc', 'theme_solent'), '', PARAM_RAW);
+    $setting = new admin_setting_scsscode('theme_solent/scss', get_string('rawscss', 'theme_boost'),
+        get_string('rawscss_desc', 'theme_boost'), '', PARAM_RAW);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
 
