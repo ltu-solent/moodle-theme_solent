18a0f3b75be14197de71571721d1cf00b28b89df
Sarah Cotton <sarah.cotton@solent.ac.uk>
2020-11-16 08:48:05 +0000

    Added wwwroot parameter
    Added wwwroot parameter

--- amd/src/headerimage.js
+++ amd/src/headerimage.js
@@ -1,11 +1,11 @@
 define(['jquery'], function($) {
 
     return {
-        init: function() {
+        init: function(wwwroot) {
 
             $("input[name='opt']").click(function(){
                 var opt = $("input[name='opt']:checked").val();
-                var url = "http://localhost/moodle36/theme/solent/set_header_image.php?";
+                var url = wwwroot + "/theme/solent/set_header_image.php?";
                 if(opt){
                     var course = document.getElementById('course').value;
                     window.location.replace(url+"course="+course+"&opt="+opt);
