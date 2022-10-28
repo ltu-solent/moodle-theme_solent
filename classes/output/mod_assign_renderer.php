d5fa13a38a6b0ed904ea505c3b5a15377fdd62c8
Mark Sharp <mark.sharp@solent.ac.uk>
2022-10-28 15:44:34 +0100

    Identical function to parent class. Removing

--- classes/output/mod_assign_renderer.php
+++ classes/output/mod_assign_renderer.php
@@ -19,33 +19,6 @@ use context_module;
 class mod_assign_renderer extends \mod_assign\output\renderer {
 
     /**
-     * Utility function to add a row of data to a table with 2 columns where the first column is the table's header.
-     * Modified the table param and does not return a value.
-     *
-     * @param html_table $table The table to append the row of data to
-     * @param string $first The first column text
-     * @param string $second The second column text
-     * @param array $firstattributes The first column attributes (optional)
-     * @param array $secondattributes The second column attributes (optional)
-     * @return void
-     */
-    private function add_table_row_tuple(html_table $table, $first, $second, $firstattributes = [],
-            $secondattributes = []) {
-        $row = new html_table_row();
-        $cell1 = new html_table_cell($first);
-        $cell1->header = true;
-        if (!empty($firstattributes)) {
-            $cell1->attributes = $firstattributes;
-        }
-        $cell2 = new html_table_cell($second);
-        if (!empty($secondattributes)) {
-            $cell2->attributes = $secondattributes;
-        }
-        $row->cells = array($cell1, $cell2);
-        $table->data[] = $row;
-    }
-
-    /**
      * Render a table containing the current status of the submission.
      *
      * @param assign_submission_status $status
