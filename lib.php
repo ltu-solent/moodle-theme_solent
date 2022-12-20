b6bfc33b93ec3c24e44bef04f1688aaf8c98da38
Mark Sharp <mark.sharp@solent.ac.uk>
2022-12-20 09:57:14 +0000

    Needed the FA4 shims

--- lib.php
+++ lib.php
@@ -97,4 +97,5 @@ function theme_solent_page_init(moodle_page $page) {
         local_solent_page_init($page);
     }
     $page->requires->css('/theme/solent/fonts/fontawesome5/css/all.min.css');
+    $page->requires->css('/theme/solent/fonts/fontawesome5/css/v4-shims.min.css');
 }
