117febd1e08fb11be82cf6d14c2065f0c0f91a57
Mark Sharp <mark.sharp@solent.ac.uk>
2023-09-20 09:30:55 +0100

    PHP8.1 compatibility - empty value in json_decode

--- classes/output/mod_assign_renderer.php
+++ classes/output/mod_assign_renderer.php
@@ -489,12 +489,14 @@ class mod_assign_renderer extends renderer_base {
         // Standard table filtering is done via query params, and these are saved in user preferences for later retrieval.
         // Get the saved options first, then check the query params to find what the latest filters should be.
         // Note the query param could be empty.
-        $prefs = json_decode(get_user_preferences('flextable_mod_assign_grading-' . $assign->get_context()->id), true);
-        if (!$prefs) {
+        $gradingprefs = get_user_preferences('flextable_mod_assign_grading-' . $assign->get_context()->id);
+        if (!$gradingprefs) {
             $prefs = [
                 'i_first'  => '',
                 'i_last'   => '',
             ];
+        } else {
+            $prefs = json_decode($gradingprefs, true);
         }
         $parammapping = ['tifirst' => 'i_first', 'tilast' => 'i_last'];
         $showprefwarning = false;
